59e3dd5568c036f95c0db8f4498f06ee7f87b213
Crisjake Sinajon <144343461+CJ4Y05@users.noreply.github.com>
2026-06-30 09:23:36 +0800

    Add string slicing example in slicing.js

---
+++ String Slicing/slicing.js
@@ -0,0 +1,10 @@
+const fullName = "crisjake sinajon";
+
+let firstName = fullName.slice(0, 9);
+let lastName = fullName.slice(9, 18);
+
+let lastChar = fullName.slice(-1);
+
+console.log(firstName);
+console.log(lastName);
+console.log(lastChar);
