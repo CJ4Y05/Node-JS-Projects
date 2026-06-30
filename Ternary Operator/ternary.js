8839ecad3b17401ad9ab265421822afeb5077218
Crisjake Sinajon <144343461+CJ4Y05@users.noreply.github.com>
2026-06-30 09:13:28 +0800

    Implement discount message using ternary operator

---
+++ Ternary Operator/ternary.js
@@ -0,0 +1,29 @@
+/* let age = 18;
+
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+
+console.log(message);
+
+
+
+let time = 16;
+
+let greeting = time < 12 ? "Good Morning!" : "Good Afternoon!";
+
+console.log(greeting);
+
+
+
+let isStudent = false;
+
+let message = isStudent? "Welcome to class!" : "Get away from me!";
+
+console.log(message);
+
+*/
+
+let purchaseAmount = 125;
+
+let message = purchaseAmount >= 100 ? "You get a 10% discount" : "You have to pay full price bitch!";
+
+console.log(message);
